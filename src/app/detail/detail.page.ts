e0f3b0d2f65ab11931a39c2b623fe60b814fbd0f
atabek54 <77456430+atabek54@users.noreply.github.com>
2022-07-25 19:44:14 +0300

    son

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
-import {  NavController } from '@ionic/angular';
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { LoadingController, ModalController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detail',
@@ -15,7 +15,7 @@ public ilan:any='';
 public telefon:any;
 public user:any='';
 
-  constructor(private http:HttpClient,private activatedRoute: ActivatedRoute,private navCtrl:NavController,private call:CallNumber) {
+  constructor(private http:HttpClient,private activatedRoute: ActivatedRoute,private navCtrl:NavController,private call:CallNumber,private loading:LoadingController) {
     this.ilanUid = this.activatedRoute.snapshot.paramMap.get('id');
     this.user=JSON.parse(localStorage.getItem('user'));
 
@@ -27,12 +27,16 @@ this.telefon=data;
 
   });
 }
-   detayGetir(){
-    this.http.get('https://webservis.online/candostum.php?servis_adi=ilan_detayi_getir&ilanUid='+this.ilanUid).subscribe(data=>{
-      this.ilan=data;
+async detayGetir(){
+  const loading=await this.loading.create();
+  loading.present();
 
-    })
-   }
+  this.http.get('https://webservis.online/candostum.php?servis_adi=ilan_detayi_getir&ilanUid='+this.ilanUid).subscribe(data=>{
+    this.ilan=data;
+
+  });
+  loading.dismiss();
+ }
   ngOnInit() {
     this.detayGetir();
     this.kullaniciTelefonGetir();
--- src/app/detaymodal/detaymodal.page.ts
+++ src/app/detaymodal/detaymodal.page.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
-import { ModalController,  } from '@ionic/angular';
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { LoadingController, ModalController,  } from '@ionic/angular';
 
 @Component({
   selector: 'app-detaymodal',
@@ -14,8 +14,9 @@ export class DetaymodalPage implements OnInit {
   public user:any=''
 public ilan:any='';
 @Input() id: string;
-  constructor(private http:HttpClient,private modalCtrl:ModalController,private call:CallNumber) {
+  constructor(private http:HttpClient,private modalCtrl:ModalController,private call:CallNumber,private loading:LoadingController) {
     this.user=JSON.parse(localStorage.getItem('user'));
+
     this.detayGetir();
    }
    kullaniciTelefonGetir(){
@@ -30,11 +31,15 @@ public ilan:any='';
     .catch(err => console.log('Error launching dialer', err));    }
 
 
-   detayGetir(){
+  async detayGetir(){
+    const loading=await this.loading.create();
+    loading.present();
+
     this.http.get('https://webservis.online/candostum.php?servis_adi=ilan_detayi_getir&ilanUid='+this.id).subscribe(data=>{
       this.ilan=data;
 
-    })
+    });
+    loading.dismiss();
    }
   ngOnInit() {
 
