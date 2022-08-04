25b9dc3ddf159a4f7e8fbe08ce5365bdd2cc4dca
Selman Atabek <selmanatabek@hotmail.com>
2022-08-04 19:42:51 +0300

    3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tabs/tab1',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
