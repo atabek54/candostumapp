0bcb1cf51452ee45054d10b7b5bce4b10eeced0f
Hüseyin Selman Atabek <77456430+atabek54@users.noreply.github.com>
2022-08-29 14:21:51 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,22 @@ export class AppComponent {
 
   //servisler
   kullanici_giris= 'url';
-  kullanici_kayit='https://webservis.online/candostum.php?servis_adi=kullanici_kayit&email=';
-  kullanici_getir='https://webservis.online/candostum.php?servis_adi=kullanici_getir&email=';
-  kullanici_bilgi_guncelle='https://webservis.online/candostum.php?servis_adi=kullanici_bilgileri_guncelle&ad_soyad=';
-  sifre_sifirla='https://webservis.online/candostum.php?servis_adi=sifre_sifirla&email=';
-  illeri_getir='https://webservis.online/candostum.php?servis_adi=illeri_getir';
-  tur_getir='https://webservis.online/candostum.php?servis_adi=tur_getir&kategori=';
-  kategori_getir='https://webservis.online/candostum.php?servis_adi=kategori_getir';
-  tum_ilanlari_getir='https://webservis.online/candostum.php?servis_adi=tum_ilanlari_getir';
-  kullanici_ilanlarini_getir='https://webservis.online/candostum.php?servis_adi=kullanicinin_ilanlarini_getir&user_id=';
-  en_yeni_ilanlari_getir='https://webservis.online/candostum.php?servis_adi=en_yeni_ilanlari_getir';
-  ilan_detay_getir='https://webservis.online/candostum.php?servis_adi=ilan_detayi_getir&ilanUid=';
-  ilan_ekle= 'https://webservis.online/candostum.php?servis_adi=ilan_ekle&ilan_turu=' ;
-  ilani_sil='https://webservis.online/candostum.php?servis_adi=ilani_sil&ilanUid=';
-  ilan_uid_kontrol=  'https://webservis.online/candostum.php?servis_adi=ilanUid_kontrolu&ilanUid=';
-  dogrulama_kodu_gonder= 'https://webservis.online/candostum.php?servis_adi=dogrulama_kodu_gonder&email=';
-  hesab_dogrulama='https://webservis.online/candostum.php?servis_adi=hesabi_dogrula&user_id=';
+  kullanici_kayit='url';
+  kullanici_getir='url';
+  kullanici_bilgi_guncelle='url';
+  sifre_sifirla='url';
+  illeri_getir='url';
+  tur_getir='url';
+  kategori_getir='url';
+  tum_ilanlari_getir='url';
+  kullanici_ilanlarini_getir='url';
+  en_yeni_ilanlari_getir='url';
+  ilan_detay_getir='url';
+  ilan_ekle='url' ;
+  ilani_sil='url';
+  ilan_uid_kontrol=  'url';
+  dogrulama_kodu_gonder= 'url';
+  hesab_dogrulama='url';
   constructor() {}
 
 
